de64415cd47935ea4cff3dd05238b62f5514ddec
vyshnavt <vyshnavt>
2022-07-10 20:02:08 +0530

    semilast

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db: null
 }
 module.exports.connect = function (done) {
-    const url ="mongodb://localhost:27017"
+    const url ="mongodb+srv://vyshnavt:vyshnav860vys@cluster0.rqelv.mongodb.net/?retryWrites=true&w=majority"
     const dbname ='brandsho'
 
     mongoClient.connect(url, (err, data) => {
